ae9f2f8071de534d65dcaeb0328bbed012255952
SparksFullStack <csparks@antioch.edu>
2018-12-06 15:01:27 -0800

    videos rendering in a list and styled--buttons being rendered and displaying; minor styling touches left to do

--- js/index.js
+++ js/index.js
@@ -50,8 +50,53 @@ function search(){
         type: 'video',
         key: 'AIzaSyDVsESHdelHRLqPZcaR5kcyQqVlMTxlW_Y',
     }, (data) => {
+        const { nextPageToken, prevPageToken } = data;
+
         // works just like forEach in vanilla JS
             // first param: array, second: callback
-        $.each(data.items, (index, item) => console.log(item));
+        $.each(data.items, (index, item) => {
+            const output = getOutput(item); // custom function to handle the data returned from the request
+
+            // displaying the results by appending them to the #results
+            $('#results').append(output);
+        });
+
+        var buttons = getButtons(prevPageToken, nextPageToken, q); // custom function to build buttons based on the results
+
+
+        console.log(buttons)
+        // displaying the buttons in the HTML
+        $('#buttons').append(buttons);
     })
+}
+
+
+// Request Output Handler
+function getOutput(item){
+    console.log('running')
+    const { videoId } = item.id;
+    const { 
+        title, 
+        description, 
+        channelTitle,
+        publishedAt 
+    } = item.snippet;
+    const thumb = item.snippet.thumbnails.high.url;
+
+    // building the output 
+    let output = '<li><div class="list-left"><img src="'+thumb+'"></div><div class="list-right"><h3>'+title+'</h3><small>By <span class="cTitle">'+channelTitle+'</span> on '+publishedAt+'</small><p>'+description+'</p></div></li><div class="clearfix"></div>';
+
+    return output;
+}
+
+// Button Creation Handler
+function getButtons(prevPageToken, nextPageToken, q){
+    let btnOutput = "";
+    if (!prevPageToken) {
+        btnOutput = '<div class="button-container"><button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'" onclick="nextPage();">Next Page</button></div>'
+    } else {
+        btnOutput = '<div class="button-container"><button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'" onclick="nextPage();">Next Page</button><button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'" onclick="prevPage();">Previous Page;</button></div>'
+    }
+
+    return btnOutput;
 }
