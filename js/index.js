e5bed835e8ac663db97edd7ca7a60b2b74b8fbb4
SparksFullStack <csparks@antioch.edu>
2018-12-06 14:27:15 -0800

    added request handling functionality that returns the results from the YouTube search

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-// AIzaSyDVsESHdelHRLqPZcaR5kcyQqVlMTxlW_Y
-
-$(document).ready(() => {
-    $('h1').addClass('red');
-})
-
 // Searchbar Handler
 $(function(){
     var searchField = $('#query');
@@ -31,5 +25,33 @@ $(function(){
                 right: '360px'
             }, 400, function(){});
         }
+    });
+
+    $('#search-form').submit(function(e){
+        e.preventDefault();
     })
 })
+
+
+// Search Function
+function search(){
+    // zeroing out the results and their buttons
+    $('#results').html("");
+    $('#buttons').html("");
+
+    // getting the value from the query element
+    var q = $(`#query`).val();
+
+
+    // making the GET request
+    $.get("https://www.googleapis.com/youtube/v3/search", {
+        part: 'snippet, id',
+        q: q,
+        type: 'video',
+        key: 'AIzaSyDVsESHdelHRLqPZcaR5kcyQqVlMTxlW_Y',
+    }, (data) => {
+        // works just like forEach in vanilla JS
+            // first param: array, second: callback
+        $.each(data.items, (index, item) => console.log(item));
+    })
+}
