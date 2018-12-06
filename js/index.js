cb4ef9e549e85a488d5ca4e5d6aa2bf1279f45f5
SparksFullStack <csparks@antioch.edu>
2018-12-06 13:45:26 -0800

    search bar animated via jQuery

--- js/index.js
+++ js/index.js
@@ -3,3 +3,33 @@
 $(document).ready(() => {
     $('h1').addClass('red');
 })
+
+// Searchbar Handler
+$(function(){
+    var searchField = $('#query');
+    var icon = $('#search-btn');
+
+    // Focus Handler
+    $(searchField).on('focus', function(){
+        $(this).animate({
+            width: '100%'
+        }, 400);
+
+        $(icon).animate({
+            right: '10px'
+        }, 400);
+    });
+
+    // Blur Handler
+    $(searchField).on('blur', function(){
+        if (searchField.val() === ""){
+            $(searchField).animate({
+                width: '45%'
+            }, 400, function(){});
+
+            $(icon).animate({
+                right: '360px'
+            }, 400, function(){});
+        }
+    })
+})
